74e01f43e450d9d9808b4132cd315b5fbccab6ca
AymanAegon <ayo12ammar24@gmail.comnode>
2024-01-06 14:41:26 +0100

    0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+pascal_triangle
+"""
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    arr = ["1"]
+    lastRow = ""
+    row = ""
+    for i in range(n - 1):
+        row = "1"
+        for j in range(len(lastRow) - 1):
+            row += str(int(lastRow[j]) + int(lastRow[j + 1]))
+        row += "1"
+        lastRow = row
+        arr.append(row)
+
+    return arr
