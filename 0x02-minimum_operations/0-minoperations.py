e020200ce826f4961f057520e2c76b134b11900d
AymanAegon <ayo12ammar24@gmail.comnode>
2024-01-18 18:36:44 +0100

    0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,33 +9,33 @@ def minOperations(n):
     calculates the fewest number of operations needed
     to result in exactly n H characters in the file.
     '''
-    pasted_chars = 1
+    current_count = 1
     clipboard = 0
-    counter = 0
+    operation_count = 0
 
-    while pasted_chars < n:
+    while current_count < n:
         if clipboard == 0:
-            clipboard = pasted_chars
-            counter += 1
+            clipboard = current_count
+            operation_count += 1
 
-        if pasted_chars == 1:
-            pasted_chars += clipboard
-            counter += 1
+        if current_count == 1:
+            current_count += clipboard
+            operation_count += 1
             continue
 
-        remaining = n - pasted_chars
+        remaining = n - current_count
         if remaining < clipboard:
             return 0
 
-        if remaining % pasted_chars != 0:
-            pasted_chars += clipboard
-            counter += 1
+        if remaining % current_count != 0:
+            current_count += clipboard
+            operation_count += 1
         else:
-            clipboard = pasted_chars
-            pasted_chars += clipboard
-            counter += 2
+            clipboard = current_count
+            current_count += clipboard
+            operation_count += 2
 
-    if pasted_chars == n:
-        return counter
+    if current_count == n:
+        return operation_count
     else:
         return 0
