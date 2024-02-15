3b1f2adfb0799523a20308b7b4b2941021cb63d4
Ayman Ammar <ayman24ammar@gmail.com>
2024-02-15 16:51:24 +0100

    0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,7 +5,7 @@ request('https://swapi-api.alx-tools.com/api/films/3', function (error, response
   if (error) {
     console.error('Error:', error);
   } else {
-    console.log(response["name"]);
+    console.log(body["name"]);
     // console.log('Body:', body);
   }
 });
