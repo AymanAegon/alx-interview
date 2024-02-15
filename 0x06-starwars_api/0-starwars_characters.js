b2775eb22b90fbb21a16d83f58ae83717e2e0c78
Ayman Ammar <ayman24ammar@gmail.com>
2024-02-15 17:17:20 +0100

    0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,11 +2,12 @@
 const request = require('request');
 
 
-request('https://swapi-api.alx-tools.com/api/films/3', function (error, response, body) {
+const film = process.argv.slice(2)[0];
+request('https://swapi-api.alx-tools.com/api/films/'+ film, function (error, response, body) {
   if (error) {
     console.error('Error:', error);
   } else {
-    characters = JSON.parse(body)["characters"];
+    let characters = JSON.parse(body)["characters"];
     characters.forEach(character => {
       request(character, function (error, response, body) {
         if (error) {
