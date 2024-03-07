3915869ebdd7a110f5a77f75a0a3de1ea0bf6323
Ayman Ammar <ayman24ammar@gmail.com>
2024-03-07 12:18:52 +0100

    fix

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -17,15 +17,18 @@ def island_perimeter(grid):
         if i != -1:
             break
     h = 1
-    w = 1
+    w = 0
     for row in range(i + 1, len(grid)):
         if grid[row][j] == 0:
             break
         h += 1
+    arr = [0] * len(grid[0])
     for row in grid:
-        k = 0
-        for e in row:
-            if e == 1:
-                k += 1
-        w = max(k, w)
+        for e in range(len(row)):
+            if row[e] == 1:
+                arr[e] = 1
+    for e in arr:
+        if e == 1:
+            w += 1
+    # return (h, w)
     return 2 * (h + w)
